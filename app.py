40dfd7e9f51ca0f43e573ad83a9cf4865278b1ff
Dioteng <jonaemazis@gmail.com>
2023-12-08 23:49:36 +0800

    ML models integrated

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
-
 import re
+import pickle
 from models import db, Users
+
+# Load the trained model and scaler from their pickle files
+with open('static/ml_models/logistic_regression.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('static/ml_models/linear_regression.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 app = Flask(__name__)
 
@@ -16,6 +23,10 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route("/")
+def home():
+    return render_template("login.html")
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
@@ -76,14 +87,72 @@ def login():
 def dashboard():
     if 'loggedin' in session:        
         return render_template("dashboard.html")
-    return redirect(url_for('login'))   
- 
+    return redirect(url_for('login'))
+
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('userid', None)
     session.pop('email', None)
     return redirect(url_for('login'))
+
+# Prediction functions and routes
+@app.route("/logistic", methods =['GET', 'POST'])
+def logistic():
+    if 'loggedin' in session:        
+        return render_template("/predict/logistic.html")
+    return redirect(url_for('login'))   
+
+@app.route("/logistic_predict", methods =['GET', 'POST'])
+def logistic_predict():
+    if 'loggedin' in session:
+        if request.method == 'POST':
+            # Get the data from the POST request.
+            Age = int(request.form['age'])
+            Hypertension = int(request.form['hypertension'])
+            Heart_Disease = int(request.form['heartdisease'])           
+            Glucose = float(request.form['glucose'])
+            BMI = float(request.form['bmi'])
+
+            # Store the inputs to the model then predict
+            prediction = model.predict([[Age, Hypertension, Heart_Disease, Glucose, BMI]])
+            
+            # For Checking the output
+            # print(prediction)
+
+            # Check if the prediction is 1 or 0
+            if prediction[0] == 1:
+                return render_template('/predict/logistic.html', prediction_text='The patient will likely to have stroke 😟')
+            else:
+                return render_template('/predict/logistic.html', prediction_text='The patient will not likely to have stroke 😃')
+        return redirect(url_for('logistic'))
+    
+@app.route("/linear", methods =['GET', 'POST'])
+def linear():
+    if 'loggedin' in session:        
+        return render_template("/predict/linear.html")
+    return redirect(url_for('login'))   
+
+@app.route("/linear_predict", methods =['GET', 'POST'])
+def linear_predict():
+    if 'loggedin' in session:
+        if request.method == 'POST':
+            # Get the data from the POST request.
+            Age = int(request.form['age'])
+            SysBP = float(request.form['sysBP'])
+            DiaBP = float(request.form['diaBP'])
+            BMI = float(request.form['bmi'])
+            HeartRate = int(request.form['heartRate'])
+            Glucose = float(request.form['glucose'])
+
+            # Store the inputs to the model then predict
+            prediction = model.predict([[Age, SysBP, DiaBP, BMI, HeartRate, Glucose]])
+            
+            # Print output
+            # print(prediction)
+
+            return render_template('/predict/linear.html', prediction_text='The total cholesterol of the patient is {:.2f}'.format(prediction[0]))
+        return redirect(url_for('linear'))
 
 if __name__ == "__main__":
     app.run(debug=True)
